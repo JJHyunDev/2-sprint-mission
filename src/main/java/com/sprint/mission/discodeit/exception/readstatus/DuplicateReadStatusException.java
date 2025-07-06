b913ca3572a25dd374385e4d8d1c134cb3d19aa8
JJHyunDev <bellhyunforgithub@gmail.com>
2025-07-06 23:36:42 +0900

    feat: different commit histories 문제로 branch 옮겨서 다시 올립니다.

--- src/main/java/com/sprint/mission/discodeit/exception/readstatus/DuplicateReadStatusException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/readstatus/DuplicateReadStatusException.java
@@ -4,15 +4,17 @@ import com.sprint.mission.discodeit.exception.ErrorCode;
 
 import java.util.UUID;
 
-public class DuplicateReadStatusException extends ReadStatusException {
-    public DuplicateReadStatusException() {
-        super(ErrorCode.DUPLICATE_READ_STATUS);
-    }
-    
-    public static DuplicateReadStatusException withUserIdAndChannelId(UUID userId, UUID channelId) {
-        DuplicateReadStatusException exception = new DuplicateReadStatusException();
-        exception.addDetail("userId", userId);
-        exception.addDetail("channelId", channelId);
-        return exception;
-    }
+public class DuplicateReadStatusException extends
+    com.sprint.mission.discodeit.exception.readstatus.ReadStatusException {
+
+  public DuplicateReadStatusException() {
+    super(ErrorCode.DUPLICATE_READ_STATUS);
+  }
+
+  public static DuplicateReadStatusException withUserIdAndChannelId(UUID userId, UUID channelId) {
+    DuplicateReadStatusException exception = new DuplicateReadStatusException();
+    exception.addDetail("userId", userId);
+    exception.addDetail("channelId", channelId);
+    return exception;
+  }
 } 
